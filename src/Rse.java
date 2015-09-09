bc121164a5d62346791fc7d99c5a8250b240d9b5
Niklas <bknjniklas@gmail.com>
2015-09-09 20:07:44 +0200

    TestRse completed and tested

--- src/Rse.java
+++ src/Rse.java
@@ -5,23 +5,23 @@ Simon Moos
  */
 public class Rse
 {
-    public static boolean allRowSumsEqual(int [][] m)
+    public static boolean allRowSumsEqual(int [][] m) //returnerar true/false för om alla rader i matrisen har samma summa
     {
-        if (m == null)
+        if (m == null)  //om matrisen är null är summorna lika
             return true;
 
         for (int k= 1; k < m.length; k++)
         {
-            if (rowSum(m[0]) != rowSum(m[k]))
+            if (rowSum(m[0]) != rowSum(m[k]))   //jämför samtliga rader med första raden
                 return false;
         }
         return true;
     }
 
-    public static int rowSum(int[] v)
+    public static int rowSum(int[] v)   //summera rader
     {
         int sum = 0;
-        if (v == null)
+        if (v == null)  //om raden är null är radens summa 0
             return 0;
 
         for (int k = 0; k < v.length; k++)
--- src/TestRse.java
+++ src/TestRse.java
@@ -5,6 +5,58 @@ public class TestRse
 {
     public static void main(String[] args)
     {
+        int [][] a = { {1, 2, 1, 2, 1, 2},      //testmatriser...
+                       {4, 0, 0, 2, 2, 1},
+                       {1, 1, 1, 1, 1, 4} };
 
+        int [][] b = { {1, 2, 3, 4},
+                       {5, 6, 7, 8},
+                       {1, 1, 1, 1} };
+
+        print(a);
+        if (Rse.allRowSumsEqual(a))
+            System.out.println("All sums are equal!");
+        else
+            System.out.println("All sums are not equal!");
+
+        print(b);
+        if (Rse.allRowSumsEqual(b))
+            System.out.println("All sums are equal!");
+        else
+            System.out.println("All sums are not equal!");
+    }
+
+    static void print(int[][] m)    //för utskrift
+    {
+        if (m == null)
+            System.out.print("[ null ]");
+
+        else if (m.length == 0)
+            System.out.print("[ - ]");
+
+        else
+        {
+            System.out.print("[ ");
+            for (int row = 0; row < m.length; row++)
+            {
+                if (m[row] == null)
+                    System.out.print("null ");
+
+                else
+                {
+                    System.out.print("{ ");
+                    if (m[row].length == 0)
+                        System.out.print("- ");
+
+                    else
+                    {
+                        for (int col = 0; col < m[row].length; col++)
+                            System.out.print(m[row][col] + " ");
+                    }
+                    System.out.print("} ");
+                }
+            }
+            System.out.println("]");
+        }
     }
 }
